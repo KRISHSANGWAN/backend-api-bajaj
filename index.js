05c57be14dc0efa3ef356de73e856cc1ed3f346a
KRISHSANGWAN <129057379+KRISHSANGWAN@users.noreply.github.com>
2025-02-21 19:06:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,7 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-
-// const app = express();
-// const cors = require('cors');
-// app.use(cors());
-
-// const ip = '192.168.59.19';  
-// const port = process.env.PORT || 3000;
-
-// app.use(bodyParser.json());
-
-// app.post('/bfhl', (req, res) => {
-//     const { data } = req.body;
-
-//     if (!Array.isArray(data)) {
-//         return res.status(400).json({ error: "Data should be an array." });
-//     }
-
-//     const numbers = data.filter(item => !isNaN(item));
-//     const alphabets = data.filter(item => /^[a-zA-Z]$/.test(item));
-    
-//     const highestAlphabet = alphabets
-//         .sort((a, b) => b.toUpperCase().localeCompare(a.toUpperCase()))[0];
-
-//     const user_id = "Abhishek_Sharma_"; 
-//     const response = {
-//         is_success: true,
-//         user_id,
-//         email: "22BCS17260@cuchd.in",
-//         roll_number: "22BCS17260",
-//         numbers,
-//         alphabets,
-//         highest_alphabet: highestAlphabet ? [highestAlphabet] : [],
-//         data
-//     };
-
-//     res.json(response);
-// });
-
-// app.get('/bfhl', (req, res) => {
-//     res.status(200).json({ operation_code: 1 });
-// });
-
-// // Bind the server to the desired IP address and port
-// app.listen(port, ip, () => {
-//     console.log(`Server is running on http://${ip}:${port}`);
-// });
-
 const express = require("express");
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 
